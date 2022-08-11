a8dc6639a2530e9513f1013bcf7d1ce3e5b6d084
asciito <ayaxdev.mx@gmail.com>
2022-08-11 18:56:29 -0500

    👔 Support for JWT

--- truehome/settings/development.py
+++ truehome/settings/development.py
@@ -73,6 +73,9 @@ TEMPLATES = [
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
 }
 
 WSGI_APPLICATION = 'truehome.wsgi.application'
--- truehome/urls.py
+++ truehome/urls.py
@@ -15,6 +15,10 @@ Including another URLconf
 """
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView
+)
 from movies.views import MovieViewSet
 
 router = routers.DefaultRouter()
@@ -22,4 +26,6 @@ router.register(r'movies', MovieViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
